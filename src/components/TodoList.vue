1a3ad056e54728c1580157f39cc7e6e26cf0f3aa
osm3rr <osm3rr@mail.com>
2024-12-14 18:13:17 -0400

    primera version de la tarea

---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+</script>
+
+<template>
+    <ul>
+        <ListItem :isChecked="true">Esta es la tarea        
+        </ListItem>
+    </ul>
+</template>
+
+<style>
+</style>
